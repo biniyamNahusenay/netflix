70258780939cb835d7943d76d7ecf43e62e589c4
biniyamNahusenay <bininahu12@gmail.com>
2024-07-22 06:41:26 -0700

    updated

--- backend/index.js
+++ backend/index.js
@@ -25,9 +25,9 @@ app.use("/api/v1/upload",uploadRoutes)
 
 const __dirname = path.resolve()
 app.use("/uploads",express.static(path.join(__dirname + "/uploads")))
-app.use(express.static(path.join(__dirname,'/frontend/dist')));
+app.use(express.static(path.join(__dirname,'/frontend')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
 app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
